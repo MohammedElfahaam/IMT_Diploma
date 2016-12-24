4d6e2595da0d9ca6692302a350ca1dbd86d720f2
melfahaa <melfahaa@CAI1-L10708.vnet.valeo.com>
2016-12-24 10:20:48 +0200

    commit

---
+++ Test_with_Raga2y/Debug/sources.mk
@@ -0,0 +1,21 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+O_SRCS := 
+PP_SRCS := 
+C_SRCS := 
+SRC_SRCS := 
+S_UPPER_SRCS := 
+OBJ_SRCS := 
+P_SRCS := 
+INC_SRCS := 
+ASM_SRCS := 
+OBJS := 
+C_DEPS := 
+EXECUTABLES := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+src \
+
